c4efad2fa7e1781f13438567f716f06383b14353
1529557169-creator <1529557169@qq.com>
2026-05-07 20:24:28 +0800

    Create UI.main.dart

---
+++ lib/UI.main.dart
@@ -0,0 +1,10 @@
+lib/
+├── main.dart
+├── pages/
+│   ├── bookshelf_page.dart
+│   ├── writing_page.dart
+│   ├── outline_page.dart
+│   ├── world_page.dart
+│   ├── ai_page.dart
+│   ├── profile_page.dart
+│   └── character_page.dart
