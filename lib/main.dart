4ae1e6f9b3f457e453f3322b0360cb0a570950b2
1529557169-creator <1529557169@qq.com>
2026-05-07 13:19:26 +0800

    lib/main.dart

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'V写作',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('V写作'),
+        ),
+        body: const Center(
+          child: Text('V写作 Android MVP'),
+        ),
+      ),
+    );
+  }
+}
